1e19d054e63f1c580b7167ebfa1c64132ebc3a1a
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-04-15 18:04:44 +0900

    default setting
    프로젝트 생성시 기본으로 세팅되는 정보

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Bring-Up
+
 24' framework team project
+
 # IT기업 전용 구인구직 사이트 프로젝트
 
 ## 프로젝트 소개
+
 IT업계를 메인타겟으로 한 구인구직 사이트입니다.
+
 ## 사용 기술
+
 - **MySQL**
 - **Python**
 - **IntelliJ** : 주로 사용할 IDE
---
+++ src/main/java/com/bring_up/bringup/BringUpApplication.java
@@ -0,0 +1,13 @@
+package com.bring_up.bringup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BringUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BringUpApplication.class, args);
+	}
+
+}
