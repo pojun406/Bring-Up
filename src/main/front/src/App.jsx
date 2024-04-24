82447b33152e887230927e8108dfa2b431b54564
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-04-24 14:34:50 +0900

    do update & Proxy Setting
    use npm audit fix --force
    made setProxy.js and Test.java
    react springboot interlock

--- src/main/front/src/App.jsx
+++ src/main/front/src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css';
 import {Link} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <Link to="/company">기업 메인</Link>
+    const [hello, setHello] = useState('')
 
-    </div>
-  );
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+            <div className="App">
+                <Link to="/company">기업 메인</Link>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/main/java/com/bring_up/bringup/Controller/Test.java
@@ -0,0 +1,13 @@
+package com.bring_up.bringup.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
