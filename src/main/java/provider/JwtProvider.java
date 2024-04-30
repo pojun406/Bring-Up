dc83084f825e487cfe602c0ab3740269efd7a83c
IT-improvement <0719improvement@naver.com>
2024-04-30 16:08:10 +0900

    Add: secretKey variable and create method in JwtProvider file

--- src/main/java/provider/JwtProvider.java
+++ src/main/java/provider/JwtProvider.java
@@ -1,4 +1,27 @@
 package provider;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
 public class JwtProvider {
+    @Value("${secret-key}")
+    private String secretKey;
+
+    public String create(String email){
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.ES256, secretKey)
+                .setSubject(email).setIssuedAt(new Date()).setExpiration(expiredDate)
+                .compact();
+        //setSubject: 주체 setIssuedAt: 생성시간 setExpiration: 만료 시간
+        return jwt;
+    }
 }
