083371464e9c39dd248c014d901fe7d755d2bafb
IT-improvement <0719improvement@naver.com>
2024-05-01 10:00:34 +0900

    Add JwtAuthenticationFilter : doFilterInternal Method

--- src/main/java/filter/JwtAuthenticationFilter.java
+++ src/main/java/filter/JwtAuthenticationFilter.java
@@ -5,6 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,9 +22,40 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
 
+            String token = parseBearerToken(request);
+
+            if(token == null){
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String email = jwtProvider.validate(token);
+
+            if(email == null){ //validate에서 SigningKey가 없거나 기간이 만료됐거나
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            AbstractAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(email, null, AuthorityUtils.NO_AUTHORITIES);
+            //인증 요청에 대한 세부정보 / 웹 인증 세부정보 소스 디테일정보 구축
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            //비어있는 콘텍스트를  만듬
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);//비어있는거에 authentication을 채워준다
+
+            SecurityContextHolder.setContext(securityContext);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        filterChain.doFilter(request, response);
     }
     private String parseBearerToken(HttpServletRequest request){
 
