fb8a520b568c36af65720d2f7ce0959db642725d
IT-improvement <0719improvement@naver.com>
2024-05-01 09:55:08 +0900

    Add JwtAuthenticationFilter : parseBearerToken method

--- src/main/java/filter/JwtAuthenticationFilter.java
+++ src/main/java/filter/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -12,5 +13,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+    }
+    private String parseBearerToken(HttpServletRequest request){
+
+        String authorization = request.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization); // hasText가 null / 공백 / whitespace가 있으면 false반환 그렇지 않으면 true 반환
+
+        if(!hasAuthorization) return null;
+
+        boolean isBearer = authorization.startsWith("Bearer "); //startsWith: 시작하는문자 확인 return boolean
+        if(!isBearer) return null;
+
+        String token = authorization.substring(7);
+        return token;
     }
 }
