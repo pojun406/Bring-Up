54b13a21a864146d04e563fccb9ab8d22a71869b
it-improvement <0719improvement@naver.com>
2024-05-02 02:44:49 +0900

    Add WebSecurityConfig : FailedAuthenticationEntryPoint Method

--- src/main/java/config/WebSecurityConfig.java
+++ src/main/java/config/WebSecurityConfig.java
@@ -45,3 +45,17 @@ public class WebSecurityConfig {
     }
 
 }
+
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("{\"code\": \"AF\",\"message\": \"Authorization Failed.\"}");
+
+    }
+
+}
