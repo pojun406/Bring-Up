13cbcfc159d39ed957bc066719e511dea2288d1d
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-03 18:14:33 +0900

    complete proxy
    use thymeleaf
    success open view
    but can't read css file
    need fix it
    
    Co-Authored-By: asowjdan <134794200+asowjdan@users.noreply.github.com>

--- src/main/java/com/bring_up/bringup/company/TestController/Test.java
+++ src/main/java/com/bring_up/bringup/company/TestController/Test.java
@@ -1,39 +1,27 @@
 package com.bring_up.bringup.company.TestController;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class Test {
 
-    private Logger log = LoggerFactory.getLogger(getClass());
-
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
+        model.addAttribute("message", "Hello, Thymeleaf!");
         return "index";
     }
 
-    @GetMapping("/api/hello")
-    public String test() {
-        return "hello";
-    }
-
     @GetMapping("/api/login")
-    public ResponseEntity<String> LoginTest(@RequestParam String exampleInputEmail1, @RequestParam String exampleInputPassword1, Model model){
+    public String LoginTest(@RequestParam String id, @RequestParam String pw, Model model){
         String ID = "admin";
         String password = "1234";
 
-
-        if(ID.equals(exampleInputEmail1) && password.equals(exampleInputPassword1)){
-            log.info("hello1");
-            return ResponseEntity.ok("http://localhost:3000/company/company_main.html");
-        }
-        else {
-            log.info("hello2");
-            return ResponseEntity.badRequest().body("false");
-        }
+        if(ID.equals(id) && password.equals(pw))
+            return "./company/company_main";
+        else
+            return "false";
     }
 }
