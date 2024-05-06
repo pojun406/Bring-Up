b7e6abcbbc2537d7f5b6553fe01cc157e22d6978
pojun406 <pojun406@naver.com>
2024-05-06 19:13:35 +0900

    del Test

--- src/main/java/com/bring_up/bringup/company/member/login/LoginController.java
+++ src/main/java/com/bring_up/bringup/company/member/login/LoginController.java
@@ -1,10 +1,27 @@
 package com.bring_up.bringup.company.member.login;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class LoginController {
+import java.util.HashMap;
+import java.util.Map;
 
+@RestController
+@RequestMapping("/api")
+public class LoginController{
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestParam String id, @RequestParam String pw) {
+        // 로그인 로직 수행
+        if ("admin".equals(id) && "1234".equals(pw)) {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Login successful");
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
 }
