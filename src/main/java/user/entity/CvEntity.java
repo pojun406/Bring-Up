4c84e26ecc93f602598f9c271eed43a451253728
it-improvement <0719improvement@naver.com>
2024-05-24 17:45:37 +0900

    Modify CvEntit : delete Setter Annotation

--- src/main/java/user/entity/CvEntity.java
+++ src/main/java/user/entity/CvEntity.java
@@ -4,10 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "CV")
