ec8e8b2368efe49c6e0a25d8b74530e4ae46a3f7
it-improvement <0719improvement@naver.com>
2024-05-24 17:48:40 +0900

    Add UserEntity : userDB columns variable

--- src/main/java/user/entity/UserEntity.java
+++ src/main/java/user/entity/UserEntity.java
@@ -1,4 +1,20 @@
 package user.entity;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
+    @Id
+    private String useremail;
+    private String userpassword;
+    private String username;
+    private String useraddress;
+    private String userphonenumber;
+    private String userbrithday;
+    private boolean freelancer;
 }
