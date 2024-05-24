17a2df2fe006b308d7a432d3cddf75094adc5537
it-improvement <0719improvement@naver.com>
2024-05-24 17:43:57 +0900

    Create UserEntity.java file

--- src/main/java/user/entity/UserEntity.java
+++ src/main/java/user/entity/UserEntity.java
@@ -1,12 +1,4 @@
 package user.entity;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserEntity {
-
 }
