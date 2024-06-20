2346b8dacfb088214c64cabfd93abf28e55f09a6
pjh5020 <123628540+pjh5020@users.noreply.github.com>
2024-06-20 16:36:51 +0900

    add : profileCard

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -1,37 +1,35 @@
-/* JavaScript */
+// JavaScript
 const showMenu = (toggleId, navbarId, bodyId) => {
     const toggle = document.getElementById(toggleId),
         navbar = document.getElementById(navbarId),
         body = document.body;
 
-    if( toggle && navbar ) {
-        toggle.addEventListener('click', ()=>{
+    if(toggle && navbar) {
+        toggle.addEventListener('click', () => {
             navbar.classList.toggle('expander');
             body.classList.toggle('expanded-sidebar'); // 바디에 클래스 추가/제거
-        })
+        });
     }
-}
+};
 
 showMenu('nav-toggle', 'navbar', 'body-pd');
 
 /* LINK ACTIVE */
-const linkColor = document.querySelectorAll('.nav__link')
+const linkColor = document.querySelectorAll('.nav__link');
 function colorLink() {
-    linkColor.forEach(l=> l.classList.remove('active'))
-    this.classList.add('active')
+    linkColor.forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
 }
-linkColor.forEach(l=> l.addEventListener('click', colorLink))
+linkColor.forEach(l => l.addEventListener('click', colorLink));
 
 /* COLLAPSE MENU */
-const linkCollapse = document.getElementsByClassName('collapse__link')
-var i
+const linkCollapse = document.getElementsByClassName('collapse__link');
+for(let i = 0; i < linkCollapse.length; i++) {
+    linkCollapse[i].addEventListener('click', function() {
+        const collapseMenu = this.parentElement.querySelector('.collapse__menu');
+        collapseMenu.classList.toggle('showCollapse');
 
-for(i=0;i<linkCollapse.length;i++) {
-    linkCollapse[i].addEventListener('click', function(){
-        const collapseMenu = this.nextElementSibling
-        collapseMenu.classList.toggle('showCollapse')
-
-        const rotate = collapseMenu.previousElementSibling
-        rotate.classList.toggle('rotate')
+        const rotate = this;
+        rotate.classList.toggle('rotate');
     });
 }
